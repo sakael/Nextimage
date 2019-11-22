c9f63afc496e4b01b6dae1a039bc199a1caefc00
Sam <samakael@Sams-iMac.local>
2019-11-22 11:40:11 +0100

    First

---
+++ src/Contracts/SampleInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Nextimage\Nextimage\Contracts;
+
+/**
+ * Interface  SampleInterface
+ *
+ * @author   Mahmoud Zalt  <mahmoud@zalt.me>
+ */
+interface SampleInterface
+{
+
+}
---
+++ src/Exceptions/ConfigFileNotFoundException.php
@@ -0,0 +1,12 @@
+<?php
+namespace Nextimage\Nextimage\Exceptions;
+
+/**
+ * Class ConfigFileNotFoundException
+ *
+ * @author  Mahmoud Zalt  <mahmoud@zalt.me>
+ */
+class ConfigFileNotFoundException extends Exception
+{
+
+}
